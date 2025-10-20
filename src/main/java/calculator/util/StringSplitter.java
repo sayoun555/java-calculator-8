171701a729e8c0e379e5a5b3faabbdc6a66dea42
sayoun555 <zzangsang523@gmail.com>
2025-10-20 18:06:26 +0900

    feat: 특수 문자 처리 기능 구현

--- src/main/java/calculator/util/StringSplitter.java
+++ src/main/java/calculator/util/StringSplitter.java
@@ -1,14 +1,17 @@
 package calculator.util;
 
 import java.util.List;
+import java.util.regex.Pattern;
 
 public class StringSplitter {
     public String[] splitByDelimiter(String input, List<String> delimiters) {
-        StringBuilder regex = new StringBuilder("[");
-        for (String delimiter : delimiters) {
-            regex.append(delimiter);
+        StringBuilder regex = new StringBuilder();
+        for (int i = 0; i < delimiters.size(); i++) {
+            if (i > 0) {
+                regex.append("|");
+            }
+            regex.append(Pattern.quote(delimiters.get(i)));
         }
-        regex.append("]");
         return input.split(regex.toString());
     }
 }
